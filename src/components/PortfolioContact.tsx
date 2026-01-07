d21455c1aa6f8c519c18485e75bc21e6ffa5243f
Kunal Jindal <kunaljindalone@gmail.com>
2026-01-07 23:19:32 +0530

    Update PortfolioContact.tsx

--- src/components/PortfolioContact.tsx
+++ src/components/PortfolioContact.tsx
@@ -210,7 +210,7 @@ const PortfolioContact = () => {
               <Button 
                 variant="outline" 
                 size="lg"
-                onClick={() => window.open('https://github.com/Kuna1jindal', '_blank')}
+                onClick={() => window.open('https://github.com/Kunaljindalone', '_blank')}
               >
                 <Github className="w-5 h-5 mr-2" />
                 View GitHub
